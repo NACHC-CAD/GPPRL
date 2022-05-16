973f8b1f9d311e510585023f3ae066ed88635347
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-05-16 00:05:37 -0400

    refactoring

--- match-cuda.py
+++ match-cuda.py
@@ -7,66 +7,83 @@ import time
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument("zip1", metavar='<Zip 1 path>', type=str,
-                    help='The first zipfile containing hashes')
-parser.add_argument("zip2", metavar='<Zip 2 path>', type=str,
-                    help='The second zipfile containing hashes')
-parser.add_argument("threshold", metavar='<Threshold>', type=float,
-                    help='The matching threshold')
 
-args = parser.parse_args()
-
-
-
-start = time.time()
-
-threshold = args.threshold
-
-results = dict()
-z1 = ZipFile(args.zip1)
-z2 = ZipFile(args.zip2)
-print(f"Matching on schemas: {z1.namelist()}")
-
-for path in z1.namelist():
-    print(f"Starting Schema {path}...")
-    with z1.open(path) as f:
-        j1 = json.load(f)
-
-    with z2.open(path) as f:
-        j2 = json.load(f)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument("zip1", metavar='<Zip 1 path>', type=str, help='The first zipfile containing hashes')
+    parser.add_argument("zip2", metavar='<Zip 2 path>', type=str, help='The second zipfile containing hashes')
+    parser.add_argument("threshold", metavar='<Threshold>', type=float, help='The matching threshold')
+    parser.add_argument("--outfile", metavar='<Output file path>', type=str, help='The outputfile', default="./results.csv", dest="outfile")
+    params = parser.parse_args()
+    return params
 
 
-    bytes_list = [base64.b64decode(x) for x in j1["clks"]]
-    with open("dataset1.bin", "wb") as f:
-        for clk in bytes_list:
-            f.write(clk)
+def match(zip1_name, zip2_name, threshold, outfile):
+    start = time.time()
+    z1 = ZipFile(zip1_name)
+    z2 = ZipFile(zip2_name)
+    print("----------------------------------------------------")
+    print("Starting match process:")
+    print("input1: " + zip1_name)
+    print("input2: " + zip2_name)
+    print("output: " + outfile)
+    print("Matching on:")
+    for path in z1.namelist():
+        print("  " + path)
+    print("----------------------------------------------------")
+    print()
+    print()
+
+    results = dict()
+
+    # for each json string (schema) in the zip file
+    for path in z1.namelist():
+        print("Starting Schema: " + path + "...")
+
+        # get the json string for the first zip
+        with z1.open(path) as f:
+            j1 = json.load(f)
+        # get the json string for the second zip
+        with z2.open(path) as f:
+            j2 = json.load(f)
+
+        # write the bytes of j1 to a file called dataset1.bin
+        bytes_list = [base64.b64decode(x) for x in j1["clks"]]
+        with open("dataset1.bin", "wb") as f:
+            for clk in bytes_list:
+                f.write(clk)
+        # write the bytes of j2 to a file called dataset1.bin
+        bytes_list = [base64.b64decode(x) for x in j2["clks"]]
+        with open("dataset2.bin", "wb") as f:
+            for clk in bytes_list:
+                f.write(clk)
+
+        # run the comparison using the gpu executable
+        result = subprocess.run(["./dice-gpu-optimized.exe", str(threshold)], cwd=os.getcwd(), capture_output=True, text=True)
+        print(result.stdout)
+
+        # update the matches file with any new matches
+        with open("./matches.csv", "r") as f:
+            r = csv.reader(f)
+            for line in r:
+                if line[1] != "-1":
+                    results[int(line[0])] = int(line[1])
+
+    # write the matches to the results file
+    with open(outfile, "w", newline="") as f:
+        w = csv.writer(f)
+        w.writerow(["dataset 1", "dataset 2"])
+        w.writerows(sorted(results.items(), key=lambda item: item[0]))
+    end = time.time()
+
+    os.remove("./matches.csv")
+    os.remove("./dataset1.bin")
+    os.remove("./dataset2.bin")
+    print("Results written to: " + outfile)
+    print(f"Finished matching in: {end - start} seconds")
 
 
+if __name__ == "__main__":
+    args = parse_args()
+    match(args.zip1, args.zip2, args.threshold, args.outfile)
 
-    bytes_list = [base64.b64decode(x) for x in j2["clks"]]
-    with open("dataset2.bin", "wb") as f:
-        for clk in bytes_list:
-            f.write(clk)
-
-
-    result = subprocess.run(["./dice-gpu-optimized.exe", str(threshold)], cwd=os.getcwd(), capture_output=True, text=True)
-    print(result.stdout)
-
-    with open("./matches.csv", "r") as f:
-        r = csv.reader(f)
-        for line in r:
-            if line[1] != "-1":
-                results[int(line[0])] = int(line[1])
-
-with open("results.csv", "w", newline="") as f:
-    w = csv.writer(f)
-    w.writerow(["dataset 1", "dataset 2"])
-    w.writerows(sorted(results.items(), key=lambda item: item[0]))
-end = time.time()
-
-os.remove("./matches.csv")
-os.remove("./dataset1.bin")
-os.remove("./dataset2.bin")
-print("Results located in results.csv")
-print(f"Finished matching in: {end - start}")
---
+++ match-example.bat
@@ -0,0 +1,2 @@
+python match-cuda.py "./test/resources/ymca.zip" "./test/resources/ncceh.zip" 0.75
+
